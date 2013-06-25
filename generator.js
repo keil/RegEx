05fdf3424818307f30b96bff5a2dd2d6d54a8db2
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-06-25 10:10:09 +0000

    refactoring
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/RegEx@23286 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- generator.js
+++ generator.js
@@ -1,3 +1,17 @@
+/*
+ * Efficient Solving of Regular Expression Inequalities 
+ *  Regular Expression Generator
+ *
+ * Copyright (c) 2013, Proglang, University of Freiburg.
+ *  http://proglang.informatik.uni-freiburg.de/
+ * All rights reserved.
+ *
+ * Author Matthias Keil
+ *  http://www.informatik.uni-freiburg.de/~keilr/
+ *
+ * $Date: 2013-05-14 08:21:34 +0200 (Tue, 14 May 2013) $
+ * $Rev: 23219 $
+ */
 (function(RegEx) {
 
 		SELF = {};
