21447119f501d7932ea7440fff9b13cb00d97ac8
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-06-25 07:58:13 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/RegEx@23246 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- replaceable.js
+++ replaceable.js
@@ -10,4 +10,24 @@
 		}
 		SELF.Replaceable = Replaceable;
 
+
+		function Cache(arg) {
+
+				var cache = new Array();
+
+				if(arg!=undefined) {
+						arg.foreach(function(key, replaceable) {
+							cache.push(replaceable);
+						});
+				}
+
+				this.add = function(newReplaceable) {
+						var newCache = new Array();
+						cache.foreach(function(key, replaceable) {
+								newCache.push(replaceable);
+						});
+						newCache.push(replaceable)
+				};
+		}
+
 })(__RegEx);
