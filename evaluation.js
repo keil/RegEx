1f73f8b0c6cc7c286775f02c834499feaab518fb
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-07-08 06:57:49 +0000

    cleanup
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/RegEx@23336 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- evaluation.js
+++ evaluation.js
@@ -21,6 +21,16 @@
 		// Result
 		//////////////////////////////////////////////////
 
+		/** Evaluation Result
+		 * @param failed sum of failed RegEx
+		 * @param ok sum of ok RegEx
+		 * @param correct sum correct failed RegEx (indeterminable)
+		 * @param max Array: depth -> max of derivations
+		 * @param avg Array: depth -> averaged sum of derivations
+		 * @param failedSet Array with failed RegEx
+		 * @param indeterminableSet Array with indeterminable RegEx
+		 *
+		 */
 		function Result(failed, ok, correct, max, avg, failedSet, indeterminableSet) {
 				this.getFailed = function() {return failed;};
 				this.getOk = function() {return ok;};
@@ -36,59 +46,50 @@
 		// Make
 		//////////////////////////////////////////////////
 
-
 		function make(statistics) {
-				// isSubset=true, isValid=false
-				var failed = 0;
-				// isSubset=true, isValid=true
-				// isSubset=false, isValid=false
-				var ok = 0;
-				// isSubset=false, isValid=true
-				var correct = 0;
+				var failedSum = 0; // isSubset=true, isValid=false
+				var okSum = 0; // isSubset=true, isValid=true OR isSubset=false, isValid=false
+				var correctSum = 0; // isSubset=false, isValid=true
 
 				var maxLUDerive = new Array();
-
+				var avgTmpLUDerive = new Array();
 				var avgLUDerive = new Array();
-
 
 				var failedSet = new Array();
 				var indeterminableSet = new Array();
 
-
-
 				statistics.foreach(function(i, statistic) {		
 						// Evaluate: Failed statistics 			
 						if(statistic.isValid()==statistic.isSubset()) { 		
-								ok++;
+								okSum++;
 						}
 						else if(!statistic.isSubset()) {
 								indeterminableSet.push(statistic);
-								correct++;
+								correctSum++;
 						}
 						else {
 								failedSet.push(statistic);
-								failed++;
+								failedSum++;
 						}
 
-						// Evaluate: max()
-						if(maxLUDerive[statistic.getDepth()]==undefined)
-							maxLUDerive[statistic.getDepth()]=0;
+				// sum of all derivations (upper and lower)
+				var sum = statistic.getLowerDerivations() + statistic.getUpperDerivations();
 
-						var sum = statistic.getLowerDerivations() + statistic.getUpperDerivations();
-						if(maxLUDerive[statistic.getDepth()]<sum)
-							maxLUDerive[statistic.getDepth()]=sum;
-
-						// Evaluate: avg()
-						if(avgLUDerive[statistic.getDepth()]==undefined)
-							avgLUDerive[statistic.getDepth()]=new Array();
-
-						var sum = statistic.getLowerDerivations() + statistic.getUpperDerivations();
-						avgLUDerive[statistic.getDepth()].push(sum);
-				});
-
+				// Evaluate: max()
+				if(maxLUDerive[statistic.getDepth()]==undefined || maxLUDerive[statistic.getDepth()]<sum) {
+						maxLUDerive[statistic.getDepth()]=sum;
+				}
 
 				// Evaluate: avg()
-				avgLUDerive.foreach(function(depth, values) {
+				if(avgTmpLUDerive[statistic.getDepth()]==undefined) {
+						avgTmpLUDerive[statistic.getDepth()]=new Array();
+				}
+				avgTmpLUDerive[statistic.getDepth()].push(sum);
+
+				});
+
+				// Evaluate: avg()
+				avgTmpLUDerive.foreach(function(depth, values) {
 						if(values==undefined) return;
 						var counter = 0;
 						var sum = 0;
@@ -99,13 +100,8 @@
 						avgLUDerive[depth] = (sum/counter);
 				});
 
-				return new Result(failed, ok, correct, maxLUDerive, avgLUDerive, failedSet, indeterminableSet);
+				return new Result(failedSum, okSum, correctSum, maxLUDerive, avgLUDerive, failedSet, indeterminableSet);
 		}
 		SELF.make = make;
 
-
-
-
-
 })(__RegEx);
-
