72b111b9aa4b2bf593ab68f3136c98dc12141041
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-12-12 08:36:58 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/RegEx@23563 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- Test/RegEx/constructor.js
+++ Test/RegEx/constructor.js
@@ -1,3 +1,18 @@
+/*
+ * Efficient Solving of Regular Expression Inequalities 
+ *  Regular Expression Generator
+ *
+ * Copyright (c) 2013, Proglang, University of Freiburg.
+ *  http://proglang.informatik.uni-freiburg.de/
+ * All rights reserved.
+ *
+ * Author Matthias Keil
+ *  http://www.informatik.uni-freiburg.de/~keilr/
+ *
+ * $Date: 2013-07-23 15:59:13 +0200 (Tue, 23 Jul 2013) $
+ * $Rev: 23389 $
+ */
+
 function testRegEx(string) {
 		__sysout("* " + string);
 		__sysout("** " + mkRegEx(string).toString() + "\n");
@@ -30,7 +45,6 @@ testRegEx("[alpha]");
 testRegEx("?");
 
 // Expressions
-// TODO, normalization
 
 testRegEx("a*");
 testRegEx("a+b");
