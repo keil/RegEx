e101c0363ed7753b111116abfd1f9b7f11f8eff1
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2013-12-12 15:35:18 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/RegEx@23572 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- RegEx/containment.js
+++ RegEx/containment.js
@@ -29,19 +29,19 @@ __RegEx.Containment = (function() {
 
 		// r <= s
 		function solve(r, s, ctx) {
-	
 
 
-			/** DISPROVE */
-						if(sub.nullable() && !(this.nullable())) return false;
 
-						/** DELETE */
-						ccExp = new Exp(arg, this);
-						if(ctx.contains(ccExp)) return true;
+				/** DISPROVE */
+				if(sub.nullable() && !(this.nullable())) return false;
 
-						/** UNFOLD */
-						else return unfold(this, arg, arg.first(), ctx.bind(ccExp));
-	
+				/** DELETE */
+				ccExp = new Exp(arg, this);
+				if(ctx.contains(ccExp)) return true;
+
+				/** UNFOLD */
+				else return unfold(this, arg, arg.first(), ctx.bind(ccExp));
+
 		}
 
 
@@ -53,148 +53,148 @@ __RegEx.Containment = (function() {
 })();
 
 
-	/** C <= C' |= true  | C=C' */
-						//						if(arg==this) return true;
-						//						/** ^ <= C' |= true  | v(C') */
-						//						else if((arg==new Empty())) return true;
-						//						/** C <= C' |= true  | n(C) */
-						//						else if(arg.isEmpty()) return true;
-						//						/** C <= C' |= true  | w(C) & !n(C') */
-						//						else if(arg.isBlank()) return true;
-						//						/** C <= C' |= true  | m(C') */
-						//						else if(this.isUniversal()) return true;
-						//
-						//						/** C <= C' |= true  | ctx(C <= C') */
-						//						ccExp = new Exp(arg, this);
-						//						if(ctx.contains(ccExp)) return true;
-						//						/** otherwise */
-						//						else return unfold(this, arg, arg.first(), ctx.bind(ccExp));
+/** C <= C' |= true  | C=C' */
+//						if(arg==this) return true;
+//						/** ^ <= C' |= true  | v(C') */
+//						else if((arg==new Empty())) return true;
+//						/** C <= C' |= true  | n(C) */
+//						else if(arg.isEmpty()) return true;
+//						/** C <= C' |= true  | w(C) & !n(C') */
+//						else if(arg.isBlank()) return true;
+//						/** C <= C' |= true  | m(C') */
+//						else if(this.isUniversal()) return true;
+//
+//						/** C <= C' |= true  | ctx(C <= C') */
+//						ccExp = new Exp(arg, this);
+//						if(ctx.contains(ccExp)) return true;
+//						/** otherwise */
+//						else return unfold(this, arg, arg.first(), ctx.bind(ccExp));
 
 
 
 //////////////////////////////////////////////////
-		//  CONTAINMENT CALCULUS
-		//  context and expressions
-		//////////////////////////////////////////////////
+//  CONTAINMENT CALCULUS
+//  context and expressions
+//////////////////////////////////////////////////
 
 
-		/** UNFOLD
-		 * @param
-		 * E >= F ::= nderive_{c \in firstc(E)} (E>=F)
-		 * {e>=f | e \in nderive_{c}(E), e \in nderive_{c}(F)}
-		 * @param E super-contract
-		 * @param F sub-contract
-		 * @param first set of first literals
-		 * @param ctx containment context 
-		 * @return true|false
-		 */
-		function unfold(E, F, first, ctx) {
+/** UNFOLD
+ * @param
+ * E >= F ::= nderive_{c \in firstc(E)} (E>=F)
+ * {e>=f | e \in nderive_{c}(E), e \in nderive_{c}(F)}
+ * @param E super-contract
+ * @param F sub-contract
+ * @param first set of first literals
+ * @param ctx containment context 
+ * @return true|false
+ */
+function unfold(E, F, first, ctx) {
 
-				// verbose - true, print output: false, do not print the output
-				var verbose  = false || APC.RegEx.config.verbose;
-				if(verbose) __sysout("\n\n\n##################################################");
-				if(verbose) __sysout("## isSuperSetOf: " + E + ">=" + F);
+		// verbose - true, print output: false, do not print the output
+		var verbose  = false || APC.RegEx.config.verbose;
+		if(verbose) __sysout("\n\n\n##################################################");
+		if(verbose) __sysout("## isSuperSetOf: " + E + ">=" + F);
 
-				var result = true;
+		var result = true;
 
-				first.foreach(function(k, literal) {
-						var nderive_E = E.nderive(literal);
-						var nderive_F = F.nderive(literal);
+		first.foreach(function(k, literal) {
+				var nderive_E = E.nderive(literal);
+				var nderive_F = F.nderive(literal);
 
-						if(verbose) __sysout("## first: " + first);
-						if(verbose) __sysout("## literal: " + literal);
-						if(verbose) __sysout("## nderive_E: " + nderive_E);
-						if(verbose) __sysout("## nderive_F: " + nderive_F);
+				if(verbose) __sysout("## first: " + first);
+				if(verbose) __sysout("## literal: " + literal);
+				if(verbose) __sysout("## nderive_E: " + nderive_E);
+				if(verbose) __sysout("## nderive_F: " + nderive_F);
 
-						result = result && nderive_E.isSuperSetOf(nderive_F, ctx);
+				result = result && nderive_E.isSuperSetOf(nderive_F, ctx);
 
-						if(verbose) __sysout("## result: " + result);
+				if(verbose) __sysout("## result: " + result);
 
-						if(!result) return result; // break
-				});
-				return result;
+				if(!result) return result; // break
+		});
+		return result;
+}
+
+
+
+/** Containment Calculus
+ * Expression: C0 <= C1
+ */
+function Exp(r, s) {
+		return {
+				/** To String
+				 * @return string
+				 */
+				toString: function() {
+						return r.toString() + "<=" + s.toString();
+				}
+		}
+}
+
+/** Containment Calculus
+ * Context: {} | <Context, Expression>
+ */
+function Ctx() {	
+		// cache array
+		var context = new StringMap();
+
+		var key = function(v) {
+				return ("\"" + v + "\"");		
 		}
 
+		return {
 
-
-		/** Containment Calculus
-		 * Expression: C0 <= C1
-		 */
-		function Exp(r, s) {
-				return {
-						/** To String
-						 * @return string
-						 */
-						toString: function() {
-								return r.toString() + "<=" + s.toString();
+				/* bind function
+				 * @param CC Expression
+				 * @return <CC Context, CC Expression>
+				 */
+				bind: function(ccExp) {
+						// clone context
+						var newCtx = new Ctx();
+						context.foreach(function(k, v) {
+								newCtx.put(v);
+						});
+						// bind new CC Expression
+						if(!newCtx.contains(ccExp)) {
+								newCtx.put(ccExp);
 						}
-				}
-		}
+						return newCtx;
+				},
 
-		/** Containment Calculus
-		 * Context: {} | <Context, Expression>
-		 */
-		function Ctx() {	
-				// cache array
-				var context = new StringMap();
-
-				var key = function(v) {
-						return ("\"" + v + "\"");		
-				}
-
-				return {
-
-						/* bind function
-						 * @param CC Expression
-						 * @return <CC Context, CC Expression>
+						/* put
+						 * @param ccExp CC Expression
+						 * $return CC Expression
 						 */
-						bind: function(ccExp) {
-								// clone context
-								var newCtx = new Ctx();
-								context.foreach(function(k, v) {
-										newCtx.put(v);
-								});
-								// bind new CC Expression
-								if(!newCtx.contains(ccExp)) {
-										newCtx.put(ccExp);
-								}
-								return newCtx;
+						put: function(ccExp) {
+								context.set(ccExp.toString(), ccExp);
+								return ccExp;
 						},
 
-								/* put
-								 * @param ccExp CC Expression
-								 * $return CC Expression
-								 */
-								put: function(ccExp) {
-										context.set(ccExp.toString(), ccExp);
-										return ccExp;
-								},
+						/* get
+						 * @param ccExp CC Expression
+						 * $return CC Expression
+						 */
+						get: function(ccExp) {
+								return context.get(ccExp.toString());
+						},
 
-								/* get
-								 * @param ccExp CC Expression
-								 * $return CC Expression
-								 */
-								get: function(ccExp) {
-										return context.get(ccExp.toString());
-								},
-
-								/* contains
-								 * @param ccExp CC Expression
-								 * $return true, if ccExp in cache, false otherwise
-								 */
-								contains: function(ccExp) {
-										return context.has(ccExp.toString());
-								}
-				}
-		};
+						/* contains
+						 * @param ccExp CC Expression
+						 * $return true, if ccExp in cache, false otherwise
+						 */
+						contains: function(ccExp) {
+								return context.has(ccExp.toString());
+						}
+		}
+};
 
 
 
 
-		//////////////////////////////////////////////////
-		// APC . Contract
-		//////////////////////////////////////////////////
-		//		RegEx.Contract.Containment = {};
-		//		RegEx.Contract.Containment.Expression = Exp;
-		//		RegEx.Contract.Containment.Context = Ctx;
+//////////////////////////////////////////////////
+// APC . Contract
+//////////////////////////////////////////////////
+//		RegEx.Contract.Containment = {};
+//		RegEx.Contract.Containment.Expression = Exp;
+//		RegEx.Contract.Containment.Context = Ctx;
 
